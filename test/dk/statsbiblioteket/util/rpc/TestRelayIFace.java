a8dbba9560d7efcaf6bfb32d8239969030dca65a
Mikkel Kamstrup Erlandsen <mke@statsbiblioteket.dk>
2008-01-14 15:42:52 +0000

    Include a test that shows that it is possible to relay RMI connections and then close the intermidiate host and still have the connection working.

---
+++ test/dk/statsbiblioteket/util/rpc/TestRelayIFace.java
@@ -0,0 +1,12 @@
+package dk.statsbiblioteket.util.rpc;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Small interface to test how relaying an RMI service works.
+ */
+public interface TestRelayIFace extends Remote {
+
+    public TestRemoteIFace getRelayedService () throws RemoteException;
+}
--- test/dk/statsbiblioteket/util/rpc/TestRemoteIFaceImpl.java
+++ test/dk/statsbiblioteket/util/rpc/TestRemoteIFaceImpl.java
@@ -6,6 +6,7 @@ import org.apache.commons.logging.LogFactory;
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.RMISecurityManager;
 
 /**
  *
@@ -22,9 +23,14 @@ public class TestRemoteIFaceImpl extends UnicastRemoteObject
         log = LogFactory.getLog (TestRemoteIFaceImpl.class);
         this.msg = msg;
 
-
-        log.info ("Creating registry on port " + 2767);
-        Registry reg = LocateRegistry.createRegistry(2767);
+        Registry reg;
+        try {
+            reg = LocateRegistry.createRegistry(2767);
+            log.info ("Created registry on port " + 2767);
+        } catch (Exception e) {
+            reg = LocateRegistry.getRegistry(2767);
+            log.info ("Found registry on port " + 2767);
+        }
 
         log.info ("Binding in registry with service on port " + 2768);
         reg.bind ("test", this);
@@ -35,5 +41,17 @@ public class TestRemoteIFaceImpl extends UnicastRemoteObject
     public String ping() {
         log.debug ("Got ping");
         return msg;
+    }
+
+    public static void main (String[] args) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new RMISecurityManager());
+        }
+
+        try {
+            new TestRemoteIFaceImpl("standalone test");
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 }
