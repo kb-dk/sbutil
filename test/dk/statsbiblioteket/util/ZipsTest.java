632511447c517c8b633cbce22aed5d99a8715c60
Toke Eskildsen <te@statsbiblioteket.dk>
2008-03-03 14:35:30 +0000

    Added half a unit-test for ZIPping with permissions

--- test/dk/statsbiblioteket/util/ZipsTest.java
+++ test/dk/statsbiblioteket/util/ZipsTest.java
@@ -79,7 +79,107 @@ public class ZipsTest extends TestCase {
         
         assertTrue("Overwriting with overwrite=false should throw a FileAlreadyExistsException", gotException);
     }
-    
+
+    /**
+     * Check that permissions on files are retained when ZIPping.
+     */
+    public void testPermissions() throws Exception {
+        // FIXME: Does this work at all?
+        File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+
+        File zipFolder = new File(tmpDir, "myzip");
+        if (zipFolder.exists()) {
+            Files.delete(zipFolder);
+        }
+        assertTrue("The source folder '" + zipFolder +
+                   "' should be created",
+                   zipFolder.mkdirs());
+        File zipFile = new File(tmpDir, "myzip.zip");
+        if (zipFile.exists()) {
+            zipFile.delete();
+        }
+        File zipDestFolder = new File(tmpDir, "unzip");
+        if (zipDestFolder.exists()) {
+            Files.delete(zipDestFolder);
+        }
+        assertTrue("The destination folder '" + zipDestFolder +
+                   "' should be created",
+                   zipDestFolder.mkdirs());
+
+        File allAccess = new File(zipFolder, "allAccess");
+        allAccess.createNewFile();
+        allAccess.setReadable(true, false);
+        allAccess.setWritable(true, false);
+//        allAccess.setExecutable(true, false);
+
+        File worldWrite = new File(zipFolder, "worldWrite");
+        worldWrite.createNewFile();
+        worldWrite.setReadable(true, true);
+        worldWrite.setWritable(true, false);
+//        worldWrite.setExecutable(true, true);
+
+        File ownerOnly = new File(zipFolder, "ownerOnly");
+        ownerOnly.createNewFile();
+        ownerOnly.setReadable(true, true);
+        ownerOnly.setWritable(true, true);
+//        ownerOnly.setExecutable(true, true);
+
+        File onlyRead = new File(zipFolder, "onlyRead");
+        onlyRead.createNewFile();
+        onlyRead.setReadable(true, false);
+        onlyRead.setWritable(false, false);
+//        onlyRead.setExecutable(false, true);
+
+        String[] FILES = new String[]{"allAccess", "worldWrite",
+                                      "ownerOnly", "onlyread"};
+
+        Zips.zip(zipFolder.toString(), zipFile.toString(), false);
+
+        assertTrue("A ZIP file should be created", zipFile.exists());
+
+        Zips.unzip(zipFile.toString(), zipDestFolder.toString(), false);
+
+        File unpacked = new File(zipDestFolder, "myzip");
+        assertTrue("The unpacked folder '" + unpacked + "' should exist",
+                   unpacked.exists());
+
+        for (String file: FILES) {
+            assertPermissionsEquals("The permissions for " + file
+                                    + " should be the same",
+                                    file, zipFolder,
+                                    new File(zipDestFolder, "myzip"));
+        }
+        System.out.println("Unfortunately the group permissions for a File "
+                           + "cannot be inspected from Java. Please compare the"
+                           + " permissions for the following files and ensure "
+                           + "that they are equal");
+        for (String file: FILES) {
+            System.out.println(new File(zipFolder, file));
+            System.out.println(new File(new File(zipDestFolder, "myzip"), 
+                                        file));
+            System.out.println("");
+        }
+
+        // TODO: Proper clean-up?
+    }
+
+    private void assertPermissionsEquals(String message, String file,
+                                         File source,
+                                         File destination) {
+        assertEquals(message,
+                     permToString(new File(source, file)),
+                     permToString(new File(destination, file)));
+    }
+
+    private String permToString(File file) {
+        StringWriter sw = new StringWriter(100);
+        sw.append("read: ").append(Boolean.toString(file.canRead()));
+        sw.append(", write: ").append(Boolean.toString(file.canWrite()));
+        // Seems like ZIP does not support executable?
+//        sw.append(", exec: ").append(Boolean.toString(file.canExecute()));
+        return sw.toString();
+    }
+
     public void testUnzipDir () throws Exception {
         // FIXME: Implement me
     }
