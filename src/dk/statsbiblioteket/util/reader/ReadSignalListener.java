514f493fd4e1e021d1f9106188537bab1220e176
Mikkel Kamstrup Erlandsen <mke@statsbiblioteket.dk>
2009-01-27 13:02:32 +0000

    Javadoc twiddling

--- src/dk/statsbiblioteket/util/reader/ReadSignalListener.java
+++ src/dk/statsbiblioteket/util/reader/ReadSignalListener.java
@@ -9,7 +9,13 @@ package dk.statsbiblioteket.util.reader;
 public interface ReadSignalListener {
 
     /**
-     * Receive a ReadSignal from the SignallingReader {@code sender}
+     * Receive a ReadSignal from the SignallingReader {@code sender}.
+     * <p/>
+     * Note that the execution speed of this method directly impacts the
+     * reading speed of the {@link SignallingReader}. The reader will not
+     * continue reading until this method has returned. This means that
+     * signal listener callbacks should generally be very light.
+     *
      * @param sender the reader generating the signal
      * @param signal the actual signal emitted
      */
