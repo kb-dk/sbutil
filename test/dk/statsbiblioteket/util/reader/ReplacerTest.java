cf8cb387d6aacac8153084ce3598dc706a022f61
Toke Eskildsen <te@statsbiblioteket.dk>
2009-02-17 15:17:49 +0000

    Unit-test: Demonstration of char vs. chars vs. String replacers relative speed for simple String => String replacement

--- test/dk/statsbiblioteket/util/reader/ReplacerTest.java
+++ test/dk/statsbiblioteket/util/reader/ReplacerTest.java
@@ -1,4 +1,4 @@
-/* $Id:$
+/* $Id$
  *
  * The Summa project.
  * Copyright (C) 2005-2008  The State and University Library
@@ -28,20 +28,17 @@ import junit.framework.TestSuite;
 
 import java.util.Map;
 import java.util.Random;
-import java.util.HashMap;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
 
 /**
- * Performc comparisons and tests of different replacers.
+ * Performs comparisons and tests of different replacers.
  */
 @SuppressWarnings({"DuplicateStringLiteralInspection"})
 @QAInfo(level = QAInfo.Level.NORMAL,
         state = QAInfo.State.IN_DEVELOPMENT,
         author = "te")
 public class ReplacerTest extends TestCase {
-//    private static Log log = LogFactory.getLog(ReplacerTest.class);
+    private static Log log = LogFactory.getLog(ReplacerTest.class);
 
     @Override
     public void setUp() throws Exception {
@@ -57,29 +54,105 @@ public class ReplacerTest extends TestCase {
         return new TestSuite(ReplacerTest.class);
     }
 
+    // 'f' => 'b'
     public void testMonkeyCharToChar() throws IOException {
         Map<String, String> rules =
                 ReplacePerformanceTest.getRangeReplacements(300, 1, 1, 1, 1);
         testMonkey(new CharReplacer(rules), rules);
     }
 
+    // 'f' => "bar"
     public void testMonkeyCharToChars() throws IOException {
         Map<String, String> rules =
                 ReplacePerformanceTest.getRangeReplacements(300, 1, 1, 0, 5);
         testMonkey(new CharArrayReplacer(rules), rules);
     }
 
+    // "foo" => "bar"
     public void testMonkeyCharsToChars() throws IOException {
         Map<String, String> rules =
                 ReplacePerformanceTest.getRangeReplacements(300, 1, 5, 0, 5);
         testMonkey(new StringReplacer(rules), rules);
     }
 
+    public void testSpeedCharsVsString() throws Exception {
+        Random random = new Random(456);
+        int RUNS = 10;
+        int[] RULES = new int[]{0, 10, 100, 1000};
+        int[] DESTINATIONS = new int[]{1, 10, 20};
+        int[] INPUT_CHARS = new int[]{10, 100, 1000, 10000, 1000000};
+
+        for (int ruleCount: RULES) {
+            for (int destinations: DESTINATIONS) {
+                for (int inputChars: INPUT_CHARS) {
+                    Map<String, String> rules =
+                            ReplacePerformanceTest.getRangeReplacements(
+                                    ruleCount, 1, 1, 1, destinations);
+                    String input = ReplacePerformanceTest.randomWord(
+                            random, 0, inputChars);
+                    TextTransformer charsT = new CharArrayReplacer(rules);
+                    TextTransformer stringT = new StringReplacer(rules);
+                    System.gc();
+                    long charsTime = 0;
+                    long stringTime = 0;
+                    for (int runs = 0 ; runs < RUNS ; runs++) {
+                        long startTime = System.currentTimeMillis();
+                        charsT.transform(input);
+                        charsTime += System.currentTimeMillis() -startTime;
+                        startTime = System.currentTimeMillis();
+                        stringT.transform(input);
+                        stringTime += System.currentTimeMillis() -startTime;
+                    }
+                    log.info("Rules: " + rules.size() + ", destination "
+                             + "lengths max: " + destinations
+                             + ", input length: " + input.length()
+                             + ", chars: " + charsTime / RUNS + "ms, Strings: "
+                             + stringTime / RUNS + "ms");
+                }
+            }
+        }
+    }
+
+    public void testSpeedCharVsChars() throws Exception {
+        Random random = new Random(456);
+        int RUNS = 10;
+        int[] RULES = new int[]{0, 10, 100, 1000};
+        int[] INPUT_CHARS = new int[]{10, 100, 1000, 10000, 1000000, 10000000};
+
+        for (int ruleCount: RULES) {
+            for (int inputChars: INPUT_CHARS) {
+                Map<String, String> rules =
+                        ReplacePerformanceTest.getRangeReplacements(
+                                ruleCount, 1, 1, 1, 1);
+                String input = ReplacePerformanceTest.randomWord(
+                        random, 0, inputChars);
+                TextTransformer charT = new CharReplacer(rules);
+                TextTransformer charsT = new CharArrayReplacer(rules);
+                System.gc();
+                long charTime = 0;
+                long charsTime = 0;
+                for (int runs = 0 ; runs < RUNS ; runs++) {
+                    long startTime = System.currentTimeMillis();
+                    charT.transform(input);
+                    charTime += System.currentTimeMillis() -startTime;
+                    startTime = System.currentTimeMillis();
+                    charsT.transform(input);
+                    charsTime += System.currentTimeMillis() -startTime;
+                }
+                log.info("Rules: " + rules.size()
+                         + ", input length: " + input.length()
+                         + ", char: " + charTime / RUNS
+                         + "ms, chars: " + charsTime / RUNS + "ms");
+            }
+        }
+    }
+
     public void testMonkey(TextTransformer transformer,
                            Map<String, String> rules) throws IOException {
         Random random = new Random(456);
-        int RUNS = 50;
-        int MAX_CHARS = 300;
+        long startTime = System.currentTimeMillis();
+        int RUNS = 100;
+        int MAX_CHARS = 500;
         for (int i = 0 ; i < RUNS ; i++) {
             String input = ReplacePerformanceTest.randomWord(
                     random, 0, MAX_CHARS);
@@ -87,6 +160,8 @@ public class ReplacerTest extends TestCase {
                          BaselineReplacerTest.getReplacedBaseline(rules, input),
                          transformer.transform(input));
         }
+        log.info("Finished " + 100 + " runs with " + rules.size()
+                 + " rules and max chars " + MAX_CHARS + " for " + transformer
+                 + " in " + (System.currentTimeMillis() - startTime) + " ms");
     }
-
 }
