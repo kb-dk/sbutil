bd42330172f20dd6db005cbdd848a77688bfd481
Mikkel Kamstrup Erlandsen <mke@statsbiblioteket.dk>
2009-09-24 06:53:19 +0000

    Test: Add test stub class for the Bytes utility module and add BytesTest to the list of tests to run from Ant

---
+++ test/dk/statsbiblioteket/util/BytesTest.java
@@ -0,0 +1,25 @@
+package dk.statsbiblioteket.util;
+
+import junit.framework.TestCase;
+
+/**
+ * FIXME: Missing class docs for dk.statsbiblioteket.util.BytesTest
+ *
+ * @author mke
+ * @since Sep 24, 2009
+ */
+public class BytesTest extends TestCase {
+
+    public void setUp() {
+
+    }
+
+    public void tearDown() {
+
+    }
+
+    public void testToHex() {
+        // FIXME: Write me
+        String hex = Bytes.toHex(new byte[]{'a'});
+    }
+}
