37303dddc1544c1022d35be577ba0e99bc0c1287
Mikkel Kamstrup Erlandsen <mke@statsbiblioteket.dk>
2009-09-24 07:47:27 +0000

    Test: Implement some unit tests for the Bytes.toHex() method

--- test/dk/statsbiblioteket/util/BytesTest.java
+++ test/dk/statsbiblioteket/util/BytesTest.java
@@ -10,16 +10,19 @@ import junit.framework.TestCase;
  */
 public class BytesTest extends TestCase {
 
-    public void setUp() {
+    public void testToHexOnMD5Digest() {
+        // Contains pairs of inputString followed by the output
+        // of the Unix command 'echo -ne "<inputString>" | md5sum'
+        String[] input = {
+                "foo", "acbd18db4cc2f85cedef654fccc4a4d8",
+                "foo\n", "d3b07384d113edec49eaa6238ad5ff00",
+                "foofoo\n", "79c509301a936b89617dab2a632c23ac"
+        };
 
-    }
-
-    public void tearDown() {
-
-    }
-
-    public void testToHex() {
-        // FIXME: Write me
-        String hex = Bytes.toHex(new byte[]{'a'});
+        for (int i = 0; i < input.length; i += 2) {
+            String test = input[i];
+            String expected = input[i+1];
+            assertEquals(expected, Bytes.toHex(Checksums.md5(test)));
+        }
     }
 }
