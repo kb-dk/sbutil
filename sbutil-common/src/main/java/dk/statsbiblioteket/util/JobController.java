7782050040cb0dcc8f6c700ce6eded21a8dc41c9
Toke Eskildsen <te@ekot.dk>
2014-02-03 16:33:07 +0100

    Feature: JobController for keeping track of Threaded jobs and their results

---
+++ sbutil-common/src/main/java/dk/statsbiblioteket/util/JobController.java
@@ -0,0 +1,238 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package dk.statsbiblioteket.util;
+
+import dk.statsbiblioteket.util.qa.QAInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * An extension of CompletionService that supports non-blocking result polling as well as non-terminating wait for
+ * all jobs to finish.
+ */
+@QAInfo(level = QAInfo.Level.NORMAL,
+        state = QAInfo.State.IN_DEVELOPMENT,
+        author = "te")
+public class JobController<R> extends ExecutorCompletionService<R> {
+    private static final int MAX_IDLE_THREADS = 20;
+
+    private final Executor executor;
+    private final AtomicInteger issued = new AtomicInteger(0);
+    private final AtomicInteger tasks = new AtomicInteger(0);
+
+    /**
+     * Shortcut for {@code JobController(maxConcurrentThreads, false)}.
+     * @param maxConcurrentThreads the maximum number of active threads.
+     */
+    public JobController(int maxConcurrentThreads) {
+        this(maxConcurrentThreads, false);
+    }
+
+    /**
+     *
+     * @param maxConcurrentThreads the maximum number of active threads.
+     * @param daemonThreads if true, exiting the main thread will end JVM execution.
+     *                      If false, active threads will finish before exiting the JVM.
+     */
+    public JobController(int maxConcurrentThreads, final boolean daemonThreads) {
+        this(new ThreadPoolExecutor(
+                maxConcurrentThreads < MAX_IDLE_THREADS ? maxConcurrentThreads : MAX_IDLE_THREADS,
+                MAX_IDLE_THREADS,
+                10, TimeUnit.MINUTES,
+                new ArrayBlockingQueue<Runnable>(100),
+                new ThreadFactory() {
+                    @Override
+                    public Thread newThread(Runnable r) {
+                        Thread t = new Thread(r);
+                        t.setDaemon(daemonThreads);
+                        return t;
+                    }
+                }));
+    }
+
+    /**
+     * @param executor a ThreadPoolExecutor is recommended as that enables {@link #getActiveCount()}.
+     */
+    public JobController(Executor executor) {
+        super(executor);
+        this.executor = executor;
+    }
+
+    /**
+     * Non-blocking.
+     * @return all finished Futures.
+     */
+    public List<Future<R>> popFinished() {
+        List<Future<R>> finished = new ArrayList<Future<R>>();
+        Future<R> f;
+        while ((f = poll()) != null) {
+            finished.add(f);
+        }
+        return finished;
+    }
+
+    /**
+     * Non-blocking.
+     * @return the result of all finished Futures.
+     */
+    public List<R> popFinishedResults() throws ExecutionException, InterruptedException {
+        List<R> finished = new ArrayList<R>();
+        Future<R> f;
+        while ((f = poll()) != null) {
+            finished.add(f.get());
+        }
+        return finished;
+    }
+
+    /**
+     * Blocking. Waits for all running tasks to finish, the returns their Futures.
+     * @return all tasks.
+     */
+    public List<Future<R>> popAll() throws InterruptedException {
+        List<Future<R>> finished = new ArrayList<Future<R>>();
+        synchronized (tasks) {
+            while (tasks.get() > 0) {
+                Future<R> future = super.take();
+                tasks.decrementAndGet();
+                popCallback(future);
+                finished.add(future);
+            }
+        }
+        return finished;
+    }
+
+    /**
+     * Blocking. Waits at most the given time for all running tasks to finish, the returns their Futures.
+     * Important: The JobController is not guaranteed to be empty after this call.
+     * @return all tasks.
+     * @throws java.lang.InterruptedException if interrupted during poll.
+     */
+    public List<Future<R>> popAll(long timeout, TimeUnit unit) throws InterruptedException {
+        List<Future<R>> finished = new ArrayList<Future<R>>();
+        synchronized (tasks) {
+            while (tasks.get() > 0) {
+                Future<R> future = super.poll(timeout, unit);
+                if (future != null) {
+                    tasks.decrementAndGet();
+                    popCallback(future);
+                    finished.add(future);
+                } else {
+                    // TODO: Silent ignore of timeout is bad but this is the design of the ExecutorCompletionService
+                    return finished;
+                }
+            }
+        }
+        return finished;
+    }
+
+    /**
+     * Called each time a task is removed from the controller. Override for special processing.
+     * @param removed the Future for a finished task.
+     */
+    @SuppressWarnings("UnusedParameters")
+    protected void popCallback(Future<R> removed) {
+        // No standard processing
+    }
+
+    @Override
+    public Future<R> submit(Callable<R> task) {
+        Future<R> future = super.submit(task);
+        tasks.incrementAndGet();
+        issued.incrementAndGet();
+        return future;
+    }
+
+    @Override
+    public Future<R> submit(Runnable task, R result) {
+        Future<R> future = super.submit(task, result);
+        tasks.incrementAndGet();
+        issued.incrementAndGet();
+        return future;
+    }
+
+    // We only synchronize takers and not submitters as we only need to guard against the queue being emptied
+    // during popAll.
+    @Override
+    public Future<R> take() throws InterruptedException {
+        Future<R> future;
+        synchronized (tasks) {
+            future = super.take();
+            tasks.decrementAndGet();
+        }
+        popCallback(future);
+        return future;
+    }
+
+    @Override
+    public Future<R> poll() {
+        Future<R> future;
+        synchronized (tasks) {
+            future = super.poll();
+            if (future != null) {
+                tasks.decrementAndGet();
+                popCallback(future);
+            }
+        }
+        return future;
+    }
+
+    @Override
+    public Future<R> poll(long timeout, TimeUnit unit) throws InterruptedException {
+        Future<R> future;
+        synchronized (tasks) {
+            future = super.poll(timeout, unit);
+            if (future != null) {
+                tasks.decrementAndGet();
+            }
+            popCallback(future);
+        }
+        return future;
+    }
+
+    /**
+     * @return the number of jobs where the status has not yet been retrieved.
+     */
+    public int getTaskCount() {
+        return tasks.get();
+    }
+
+    /**
+     * @return the number of running jobs if the executor is a ThreadPoolExecutor.
+     * @throws java.lang.IllegalStateException if the executor is not a ThreadPoolExecutor.
+     */
+    public int getActiveCount() throws IllegalStateException {
+        if (!(executor instanceof ThreadPoolExecutor)) {
+            throw new IllegalStateException("The executor was a " + executor.getClass().getCanonicalName()
+                                            + " but must be a ThreadPoolExecutor for this call to succeed");
+        }
+        return ((ThreadPoolExecutor)executor).getActiveCount();
+    }
+
+    /**
+     * @return the total number of issued tasks.
+     */
+    public int getIssued() {
+        return issued.get();
+    }
+
+    @Override
+    public String toString() {
+        return "JobController(tasks=" + getTaskCount() + ", active=" + getActiveCount()
+               + ", issued=" + getIssued() + ", executor=" + executor + ")";
+    }
+}
---
+++ sbutil-common/src/test/java/dk/statsbiblioteket/util/JobControllerTest.java
@@ -0,0 +1,143 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package dk.statsbiblioteket.util;
+
+import dk.statsbiblioteket.util.qa.QAInfo;
+import junit.framework.TestCase;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ */
+@QAInfo(level = QAInfo.Level.NORMAL,
+        state = QAInfo.State.IN_DEVELOPMENT,
+        author = "te")
+public class JobControllerTest extends TestCase {
+    private static Log log = LogFactory.getLog(JobControllerTest.class);
+
+    public void testPopFinished() throws Exception {
+        final int JOBS = 10;
+        final AtomicInteger counter = new AtomicInteger(0);
+        JobController<Long> controller = new JobController<Long>(10) {
+            @Override
+            protected void popCallback(Future<Long> removed) {
+                counter.incrementAndGet();
+            }
+        };
+        for (int i = 0 ; i < JOBS ; i++) {
+            synchronized (Thread.currentThread()) {
+                Thread.currentThread().wait(10);
+            }
+            controller.submit(new Shout(50));
+        }
+        int first = controller.popFinished().size();
+        synchronized (Thread.currentThread()) {
+            Thread.currentThread().wait(60);
+        }
+        int second = controller.popFinished().size();
+
+        assertTrue("The first pop should yield some results. Got " + first, first > 0);
+        assertTrue("The second pop should yield some results. Got " + second, second > 0);
+        log.info("first popFinished: " + first + ", second popFinished: " + second);
+        assertEquals("The total pops should be correct", 10, first + second);
+        assertEquals("The callback count should be correct", 10, counter.get());
+    }
+
+    public void testPopAll() throws Exception {
+        final int JOBS = 10;
+        final AtomicInteger counter = new AtomicInteger(0);
+        JobController<Long> controller = new JobController<Long>(10) {
+            @Override
+            protected void popCallback(Future<Long> removed) {
+                counter.incrementAndGet();
+            }
+        };
+        for (int i = 0 ; i < JOBS ; i++) {
+            synchronized (Thread.currentThread()) {
+                Thread.currentThread().wait(10);
+            }
+            controller.submit(new Shout(50));
+        }
+        int all = controller.popAll().size();
+
+        assertEquals("The total pops should be correct", 10, all);
+        assertEquals("The callback count should be correct", 10, counter.get());
+    }
+
+    public void testPopTimeout() throws Exception {
+        final int JOBS = 10;
+        final AtomicInteger counter = new AtomicInteger(0);
+        JobController<Long> controller = new JobController<Long>(10) {
+            @Override
+            protected void popCallback(Future<Long> removed) {
+                counter.incrementAndGet();
+            }
+        };
+        for (int i = 0 ; i < JOBS ; i++) {
+            synchronized (Thread.currentThread()) {
+                Thread.currentThread().wait(10);
+            }
+            controller.submit(new Shout(50));
+        }
+        int allTimeout = controller.popAll(10, TimeUnit.MILLISECONDS).size();
+        int allLeft = controller.popAll().size();
+
+        assertTrue("Timeout popAll should be > 0 and < " + JOBS + " but was " + allTimeout,
+                   allTimeout > 0 && allTimeout < JOBS);
+        assertEquals("The total pops should be correct", 10, allTimeout + allLeft);
+        assertEquals("The callback count should be correct", 10, counter.get());
+    }
+
+    public void testRemoveCallback() throws Exception {
+        final int JOBS = 10;
+        final AtomicInteger counter = new AtomicInteger(0);
+        JobController<Long> controller = new JobController<Long>(10) {
+            @Override
+            protected void popCallback(Future<Long> removed) {
+                counter.incrementAndGet();
+            }
+        };
+        for (int i = 0 ; i < JOBS ; i++) {
+            controller.submit(new Shout(10));
+        }
+        synchronized (Thread.currentThread()) {
+            Thread.currentThread().wait(100);
+        }
+        assertEquals("The number of pops should match", JOBS, controller.popAll().size());
+        assertEquals("The number of callbacks should match", JOBS, counter.get());
+    }
+
+    private class Shout implements Callable<Long> {
+        private final long wait;
+
+        private Shout(long wait) {
+            this.wait = wait;
+        }
+
+        @Override
+        public Long call() throws Exception {
+            synchronized (Thread.currentThread()) {
+                Thread.currentThread().wait(wait);
+            }
+            return System.currentTimeMillis();
+        }
+    }
+}
